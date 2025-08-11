7df86413449d670c5d9f58826a659819105eb67b
kbriyad007 <kb.riyad007@gmail.com>
2025-08-11 21:09:50 +0600

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["a.storyblok.com"],
+    domains: ["a.storyblok.com", "picsum.photos"],
   },
 };
 
